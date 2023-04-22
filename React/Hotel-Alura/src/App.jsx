e3d1a25f087423c5179d0e889134df270bc8d274
CesarMauricioRojas <mauricio2328cr@gmail.com>
2023-04-22 11:54:08 -0500

    Cambios estilos

--- React/Hotel-Alura/src/App.jsx
+++ React/Hotel-Alura/src/App.jsx
@@ -1,5 +1,4 @@
 import { Route, Routes } from 'react-router-dom'
-import './App.css'
 import Home from './Pages/Home'
 import Login from './Pages/Login'
 import MenuUsuario from './Pages/MenuUsuario'
